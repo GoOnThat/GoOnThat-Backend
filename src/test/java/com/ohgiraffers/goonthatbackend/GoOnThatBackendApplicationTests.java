440beefb5f472e347ef0427e57efad88b6ac1856
taedyv <taedyoverflow@gmail.com>
2023-07-20 17:00:40 +0900

    test 클래스 추가

--- src/test/java/com/ohgiraffers/goonthatbackend/GoOnThatBackendApplicationTests.java
+++ src/test/java/com/ohgiraffers/goonthatbackend/GoOnThatBackendApplicationTests.java
@@ -8,6 +8,7 @@ class GoOnThatBackendApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println("anytest");
     }
 
 }
---
+++ src/test/java/com/ohgiraffers/goonthatbackend/anytest.java
@@ -0,0 +1,4 @@
+package com.ohgiraffers.goonthatbackend;
+
+public class anytest {
+}
