2422ec823699e738c5a5a11e22a0eb601dd743bb
LAPTOP-O91Q221D\user <pyunghun120@gmail.com>
2023-07-22 17:06:27 +0900

    [파일 Service, Controller 구성]

--- src/main/java/com/ohgiraffers/goonthatbackend/metamate/file/command/application/controller/FileController.java
+++ src/main/java/com/ohgiraffers/goonthatbackend/metamate/file/command/application/controller/FileController.java
@@ -13,7 +13,7 @@ public class FileController {
 
     private final FileService fileService;
 
-    @GetMapping("/upload")
+
 
 
 }
--- src/main/java/com/ohgiraffers/goonthatbackend/metamate/file/command/application/service/FileService.java
+++ src/main/java/com/ohgiraffers/goonthatbackend/metamate/file/command/application/service/FileService.java
@@ -1,4 +1,39 @@
 package com.ohgiraffers.goonthatbackend.metamate.file.command.application.service;
 
-public interface FileService {
+import com.ohgiraffers.goonthatbackend.metamate.file.command.application.dto.FileDTO;
+import com.ohgiraffers.goonthatbackend.metamate.file.command.domain.aggregate.entity.File;
+import com.ohgiraffers.goonthatbackend.metamate.file.command.infra.repository.FileRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class FileService {
+
+    private FileRepository fileRepository;
+
+    public FileService(FileRepository fileRepository) {
+        this.fileRepository = fileRepository;
+    }
+
+//    @Transactional
+//    public Long saveFile(FileDTO fileDTO) {
+//        return fileRepository.save(fileDTO.toEntity().getFileNo());
+//    }
+
+    @Transactional
+    public FileDTO getFile(Long fileNo) {
+        File file = fileRepository.findById(fileNo).get();
+
+        FileDTO fileDTO = FileDTO.builder()
+                .fileNo(fileNo)
+                .originFileName(file.getOriginFileName())
+                .filename(file.getFilename())
+                .filePath(file.getFilePath())
+                .metaUser(file.getMetaUser())
+                .freeBoardPost(file.getFreeBoardPost())
+                .build();
+
+        return fileDTO;
+    }
+
 }
