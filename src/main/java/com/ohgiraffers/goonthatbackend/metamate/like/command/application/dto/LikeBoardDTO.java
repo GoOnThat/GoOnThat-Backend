0709320626f2baa274fda96449ebbc20538650c2
minho jeong <wjdalsgh6951@gmail.com>
2023-07-26 14:55:10 +0900

    좋아요 커밋

--- src/main/java/com/ohgiraffers/goonthatbackend/metamate/like/command/application/dto/LikeBoardDTO.java
+++ src/main/java/com/ohgiraffers/goonthatbackend/metamate/like/command/application/dto/LikeBoardDTO.java
@@ -4,27 +4,35 @@ import com.ohgiraffers.goonthatbackend.metamate.comment.command.domain.aggregate
 import com.ohgiraffers.goonthatbackend.metamate.domain.user.MetaUser;
 import com.ohgiraffers.goonthatbackend.metamate.freeboard.command.domain.aggregate.entity.FreeBoardPost;
 import com.ohgiraffers.goonthatbackend.metamate.like.command.domain.aggregate.entity.Like;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Setter
 @Getter
 @ToString
 public class LikeBoardDTO {
 
     private Long likeNo;
     private FreeBoardPost freeBoardPost;
-    private MetaUser metaUser;
-    private boolean likeStatus;
+    private String likeMan;
+//    private boolean likeStatus;
 
     public Like toEntity(MetaUser user, FreeBoardPost freeBoardPost) {
         return Like.builder()
-                .freeBoardPost(this.freeBoardPost)
+                .freeBoardPost(freeBoardPost)
                 .metaUser(user)
 //                .likeStatus(true)
                 .build();
+
+    }
+
+    public static LikeBoardDTO fromEntity(Like like) {
+
+        return new LikeBoardDTO(
+                like.getLikeNo(),
+                like.getFreeBoardPost(),
+                like.getMetaUser().getNickname()
+        );
     }
 }
--- src/main/java/com/ohgiraffers/goonthatbackend/metamate/like/command/domain/aggregate/entity/Like.java
+++ src/main/java/com/ohgiraffers/goonthatbackend/metamate/like/command/domain/aggregate/entity/Like.java
@@ -1,39 +1,20 @@
 package com.ohgiraffers.goonthatbackend.metamate.like.command.domain.aggregate.entity;
-
-import com.ohgiraffers.goonthatbackend.metamate.comment.command.domain.aggregate.entity.FreeBoardComment;
 import com.ohgiraffers.goonthatbackend.metamate.domain.AuditingFields;
 import com.ohgiraffers.goonthatbackend.metamate.domain.user.MetaUser;
 import com.ohgiraffers.goonthatbackend.metamate.freeboard.command.domain.aggregate.entity.FreeBoardPost;
-//import com.ohgiraffers.goonthatbackend.metamate.like.command.domain.aggregate.vo.LikeVO;
+
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
-
 import javax.persistence.*;
-import java.io.Serializable;
 
-@Entity(name = "Like")
-@Table(name = "LIKE")
-@NoArgsConstructor
+
+@Entity
+@Table(name = "to_like")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class Like implements Serializable {
-
-//    @EmbeddedId
-//    private LikeVO like;
-//
-//    @JoinColumn(name = "id")
-//    private MetaUser metauser;
-//
-//    public Like(LikeVO like) {
-//        this.like = like;
-//    }
-//
-//    public Like(long userId, long boardId) {
-//        this.like = new LikeVO(userId, boardId);
-//    }
+public class Like {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -44,36 +25,39 @@ public class Like implements Serializable {
     @JoinColumn(name = "board_no")
     private FreeBoardPost freeBoardPost;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "comment_no")
-    private FreeBoardComment freeBoardComment;
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "comment_no")
+//    private FreeBoardComment freeBoardComment;
 
-    @Column
-    private boolean likeStatus;
-
-    @Column
-    private int liked;
+//    @Column
+//    private boolean likeStatus;
+//
+//    @Column
+//    private int liked;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id")
-    @OnDelete(action = OnDeleteAction.CASCADE)
     private MetaUser metaUser;
 
+//    @Column
+//    private String likeMan;
+
     @Builder
-    public Like(FreeBoardPost freeBoardPost, MetaUser metaUser) {
+    public Like(Long likeNo, FreeBoardPost freeBoardPost, MetaUser metaUser,
+                boolean likeStatus, int liked, String likeMan) {
         this.likeNo = likeNo;
         this.freeBoardPost = freeBoardPost;
-        this.freeBoardComment = freeBoardComment;
         this.metaUser = metaUser;
-        this.likeStatus = true;
-        this.liked = 0;
+//        this.likeStatus = likeStatus;
+//        this.liked = liked;
+//        this.likeMan = likeMan;
     }
-//
+
 //    public void unLikeBoard(FreeBoardPost freeBoardPost) {
 //        this.likeStatus = false;
 //        freeBoardPost.setLiked(freeBoardPost.getLiked() - 1);
 //    }
-
+//
 //    public void increaseLikeCount() {
 //        this.liked += 1;
 //    }
